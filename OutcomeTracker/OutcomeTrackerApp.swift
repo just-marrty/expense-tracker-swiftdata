489fe14c9b0c86ad52b4ffc96aacd8dbdb63ea93
Martin <marrty.h.1992@gmail.com>
2026-01-12 12:30:35 +0100

    Initial Commit

---
+++ OutcomeTracker/OutcomeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutcomeTrackerApp.swift
+//  OutcomeTracker
+//
+//  Created by Martin Hrbáček on 12.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct OutcomeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
